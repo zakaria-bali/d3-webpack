bfc37c5a000f34971daeb54b9a2865d20d901d6c
Bali <zakaria99bali@gmail.com>
2023-08-26 23:17:32 +0100

    simple transition

--- src/index.ts
+++ src/index.ts
@@ -1,28 +1,8 @@
 import * as d3 from 'd3';
 
-const body = d3.select('body')
-
-body.on('click', (event) => {
-    body.classed('clicked', true)
-}) 
-
-
-setTimeout(() => {
-
-    d3.select('circle')
+d3.select('circle')
 .transition()
 .duration(3000)
 .attr('r', 100);
-
-setTimeout(
-    () => {
-        d3.select('circle')
-    .transition()
-    .duration(3000)
-    .attr('r', 50)
-    }, 3000
-)
-
-}, 2000)
 
 
