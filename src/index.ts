edbce6c4f6f7c24d994b6af62f30b894104ebf44
Bali <zakaria99bali@gmail.com>
2023-08-26 12:51:44 +0100

    feat(modifying-elements): update the circle radius

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,28 @@
 import * as d3 from 'd3';
 
+const body = d3.select('body')
+
+body.on('click', (event) => {
+    body.classed('clicked', true)
+}) 
+
+
+setTimeout(() => {
+
+    d3.select('circle')
+.transition()
+.duration(3000)
+.attr('r', 100);
+
+setTimeout(
+    () => {
+        d3.select('circle')
+    .transition()
+    .duration(3000)
+    .attr('r', 50)
+    }, 3000
+)
+
+}, 2000)
 
 
