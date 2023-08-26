22826c83c8456855a62e9a60dd44d94a69e44b1f
Bali <zakaria99bali@gmail.com>
2023-08-26 23:39:54 +0100

    data join example

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,17 @@
 import * as d3 from 'd3';
 
 
+const data = [ 40, 10, 20, 60, 30 ]
+
+d3.select('svg')
+    .selectAll('circle')
+    .data(data)
+    .join('circle')
+    .attr('cx', (d, i) => {
+        return 50 + i*100;
+    })
+    .attr('cy', 100)
+    .attr('r', (d, i) => {
+        return d
+    })
 
