e1ccab550eb800aff5461c494611d1a4f55d991a
Zakaria Bali <z.bali@henceforth.info>
2023-08-29 10:39:52 +0100

    simple chord diagram

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,21 @@
 import * as d3 from 'd3';
 
+const data = [
+    [10, 20, 30],
+    [40, 60, 80],
+    [100, 200, 300]
+]
 
+const chordGenerator = d3.chord();
+
+const chords = chordGenerator(data)
+console.log(chords)
+
+const ribbonGenerator: any = d3.ribbon().radius(200)
+
+d3.select('g')
+.selectAll('path')
+.data(chords)
+.join('path')
+.attr('d', ribbonGenerator)
 
